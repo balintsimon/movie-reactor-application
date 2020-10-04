e203a38b4036c1e5f2b21862e86fcff4e2f6bac2
dr. Simon Bálint <balladin0@gmail.com>
2020-10-04 19:34:16 +0200

    Add link for user to log in from modal
    If user is not logged in but wants to reserve seats, modal message promps user to either log in or register to the service. These now act as a link to the login and register pages, while manage to close the prompting modal at the same time.

--- src/components/seating_page/ReserveSeatButton.js
+++ src/components/seating_page/ReserveSeatButton.js
@@ -2,15 +2,37 @@ import React, { useState} from "react";
 import "./ReserveSeatButton.css";
 import axios from "axios";
 import {API_RESERVATION_URL, OCCUPIED_SEAT_CLASS, OWN_RESERVED_SEAT_CLASS, REACTOR_YELLOW} from "../../Constants";
+import {Link} from "react-router-dom";
 
 const ReserveSeatButton = (props) => {
   let id = props.showId;
   let [modalMessage, setModalMessage] = useState("");
-  let userNotLoggedInMessage = `Please log in to reserve seats.`;
+  let userNotLoggedInMessage =
+      <div>Please
+      <Link
+          to="/auth/login"
+          id="button_to_login"
+          key="button_to_login"
+          onClick={dismissModal}
+          style={{color: "white"}}
+      > log in
+      </Link> to reserve seats.
+      <br/>If you do not have an account, please <Link
+          to="/auth/register"
+          id="button_to_register"
+          key="button_to_register"
+          onClick={dismissModal}
+          style={{color: "white"}}
+      > register
+      </Link> first.</div>;
   let noSeatsSelectedMessage = `Please select seats for reservation.`
   let successfulPurchaseMessage = `Thank you for reserving seats for '${props.movieTitle}'! 
                                     Your reservation was successful.`;
   let failedPurchaseMessage = `Sorry, we can't fulfill your reservation for '${props.movieTitle}' at the moment.`;
+
+  function dismissModal() {
+      document.getElementById("closeButton").click();
+  }
 
   function reserveSeat(event) {
     if (!localStorage.getItem("username")) {
@@ -56,7 +78,7 @@ const ReserveSeatButton = (props) => {
                 <h5 className="modal-title" id="exampleModalLabel">
                   {`Reservation for ${props.movieTitle}`}
                 </h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" id="closeButton" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
