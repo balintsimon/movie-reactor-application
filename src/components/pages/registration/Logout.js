99eb7a3878a97575fdd2e799555b63843445302a
dr. Simon Bálint <balladin0@gmail.com>
2020-10-15 00:59:36 +0200

    Delete unused import

--- src/components/pages/registration/Logout.js
+++ src/components/pages/registration/Logout.js
@@ -1,11 +1,9 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
 import axios from "axios";
 import "./../../../Constants";
 import {API_AUTHENTICATION} from "../../../Constants";
 
 const Logout = () => {
-    const history = useHistory();
 
     const logout = () => {
         localStorage.clear();
