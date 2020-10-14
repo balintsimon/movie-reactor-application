db08592559c49af4b691caf39a71d456ed4011e0
dr. Simon Bálint <balladin0@gmail.com>
2020-10-14 18:30:59 +0200

    Delete unused import

--- src/components/pages/registration/LoginPage.js
+++ src/components/pages/registration/LoginPage.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {useHistory} from "react-router-dom";
 import { Link as RouterLink } from "react-router-dom";
 import {API_AUTHENTICATION} from "../../../Constants";
 import "./Authentication.css";
