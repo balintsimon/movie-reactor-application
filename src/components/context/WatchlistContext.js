071b189490ef00ac015573ab12682f4f4b1c0ff2
dr. Simon Bálint <balladin0@gmail.com>
2020-10-15 13:19:56 +0200

    Fix watchlist to get list if user is logged in

--- src/components/context/WatchlistContext.js
+++ src/components/context/WatchlistContext.js
@@ -8,9 +8,11 @@ export const WatchlistProvider = (props) => {
   const [watchlist, setWatchlist] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(API_WATCHLIST, {withCredentials: true})
-      .then((response) => setWatchlist(response.data.watchlist));
+    if (localStorage.getItem("username")) {
+      axios
+          .get(API_WATCHLIST, {withCredentials: true})
+          .then((response) => setWatchlist(response.data.watchlist));
+    }
   }, []);
 
   return (
