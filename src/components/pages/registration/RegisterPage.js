7283509921ed0ef74a9d116d8cf8e2cc3513d02e
dr. Simon Bálint <balladin0@gmail.com>
2020-10-28 18:03:33 +0100

    Add user name field check for length

--- src/components/pages/registration/RegisterPage.js
+++ src/components/pages/registration/RegisterPage.js
@@ -5,10 +5,6 @@ import "./Register.css";
 import { API_AUTHENTICATION } from "../../../Constants";
 
 const RegisterPage = () => {
-    let passwordField = document.getElementById("password");
-
-
-
     let usernameFieldMessage = "Must be at least 6 characters long.";
     let passwordFieldMessage = "Must contain at least a number, " +
         "an upper and a lower case letter " +
@@ -44,51 +40,68 @@ const RegisterPage = () => {
     };
 
     function displayPswdCheckMessage() {
-        document.getElementById("message").style.display = "block";
+        document.getElementById("pswd_message").style.display = "block";
     }
 
     function hidePswdCheckMessage() {
-        document.getElementById("message").style.display = "none";
+        document.getElementById("pswd_message").style.display = "none";
+    }
+
+    function displayUsernameMessage() {
+        document.getElementById("username_message").style.display = "block";
+    }
+
+    function hideUsernameCheckMessage() {
+        document.getElementById("username_message").style.display = "none";
+    }
+
+    function checkUsernameContent() {
+        let usernameLength = 6;
+        let lengthField = document.getElementById("username_length");
+        let usernameField = document.getElementById("username");
+        checkFieldLength(usernameLength, lengthField, usernameField);
     }
 
     function checkPswdContent() {
         let lowerCaseLetters = /[a-z]/g;
         let capitalLetters = /[A-Z]/g;
         let numbers = /[0-9]/g;
+        let pswdLength = 8;
         let lowerCaseLetterCheckField = document.getElementById("letter");
         let capitalLetterCheckField = document.getElementById("capital");
         let numberCheckField = document.getElementById("number");
+        let length = document.getElementById("length");
+        let passwordField = document.getElementById("password");
 
-        checkPasswordContains(lowerCaseLetters, lowerCaseLetterCheckField);
-        checkPasswordContains(capitalLetters, capitalLetterCheckField);
-        checkPasswordContains(numbers, numberCheckField);
-        checkPasswordLength();
+        checkFieldContains(lowerCaseLetters, lowerCaseLetterCheckField, passwordField);
+        checkFieldContains(capitalLetters, capitalLetterCheckField, passwordField);
+        checkFieldContains(numbers, numberCheckField, passwordField);
+        checkFieldLength(pswdLength, length, passwordField);
         hidePasswordLabel();
     }
 
-    function checkPasswordContains(regex, field) {
-        if (passwordField.value.match(regex)) {
-            field.classList.remove("invalid");
-            field.classList.add("valid");
+    function checkFieldContains(regex, fieldToModify, fieldToWatch) {
+        if (fieldToWatch.value.match(regex)) {
+            fieldToModify.classList.remove("invalid");
+            fieldToModify.classList.add("valid");
         } else {
-            field.classList.remove("valid");
-            field.classList.add("invalid");
+            fieldToModify.classList.remove("valid");
+            fieldToModify.classList.add("invalid");
         }
     }
 
-    function checkPasswordLength() {
-        let pswdLength = 8;
-        let length = document.getElementById("length");
-        if (passwordField.value.length >= pswdLength) {
-            length.classList.remove("invalid");
-            length.classList.add("valid");
+    function checkFieldLength(pswdLength, fieldToModify, fieldToWatch) {
+        if (fieldToWatch.value.length >= pswdLength) {
+            fieldToModify.classList.remove("invalid");
+            fieldToModify.classList.add("valid");
         } else {
-            length.classList.remove("valid");
-            length.classList.add("invalid");
+            fieldToModify.classList.remove("valid");
+            fieldToModify.classList.add("invalid");
         }
     }
 
     function hidePasswordLabel() {
+        let passwordField = document.getElementById("password");
         let field = document.getElementById("passwordlabel");
         if (passwordField.value.length !== 0) {
             field.style.display = "none";
@@ -103,11 +116,23 @@ const RegisterPage = () => {
         <div className="register">
             <form className="register-form" onSubmit={sendRequest}>
                 <div>
-                    <input className="input-container" id="firstname" type="text" onChange={event => setFirstname(event.target.value)} required/>
+                    <input
+                        className="input-container"
+                        id="firstname"
+                        type="text"
+                        onChange={event => setFirstname(event.target.value)}
+                        required
+                    />
                     <label for="firstname">first name</label>
                 </div>
                 <div>
-                    <input className="input-container" id="lastname" type="text" onChange={event => setLastname(event.target.value)} required/>
+                    <input
+                        className="input-container"
+                        id="lastname"
+                        type="text"
+                        onChange={event => setLastname(event.target.value)}
+                        required
+                    />
                     <label for="lastname">last name</label>
                 </div>
                     <select className="dropdown" defaultValue="MAN" name="gender" id="gender">
@@ -116,39 +141,53 @@ const RegisterPage = () => {
                         <option value="GENERAL">PREFER NOT TO SAY</option>
                     </select>
                 <div>
-                    <input className="input-container" id="email" type="text" onChange={event => setEmail(event.target.value)} required/>
+                    <input
+                        className="input-container"
+                        id="email"
+                        type="text"
+                        onChange={event => setEmail(event.target.value)}
+                        required
+                    />
                     <label for="email">e-mail</label>
                 </div>
                 <div>
-                    <input className="input-container"
-                           id="username"
-                           // pattern="(?=.*[a-z]).{6,}"
-                           title={usernameFieldMessage}
-                           type="text"
-                           onChange={event => setUsername(event.target.value)}
-                           required
+                    <input
+                        className="input-container"
+                       id="username"
+                       // pattern="(?=.*[a-z]).{6,}"
+                       title={usernameFieldMessage}
+                       type="text"
+                       onChange={event => setUsername(event.target.value)}
+                       onFocus={displayUsernameMessage}
+                       onBlur={hideUsernameCheckMessage}
+                       onKeyUp={checkUsernameContent}
+                       required
                     />
                     <label for="username">user name</label>
                 </div>
                 <div>
-                    <input className="input-container"
-                           id="password"
-                           pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                           title={passwordFieldMessage}
-                           type="password"
-                           onChange={event => setPassword(event.target.value)}
-                           onFocus={displayPswdCheckMessage}
-                           onBlur={hidePswdCheckMessage}
-                           onKeyUp={checkPswdContent}
-                           required
+                    <input
+                        className="input-container"
+                       id="password"
+                       pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                       title={passwordFieldMessage}
+                       type="password"
+                       onChange={event => setPassword(event.target.value)}
+                       onFocus={displayPswdCheckMessage}
+                       onBlur={hidePswdCheckMessage}
+                       onKeyUp={checkPswdContent}
+                       required
                     />
                     <label id="passwordlabel" for="password">password</label>
                 </div>
                 <input type="submit" value="Register" className="button"/>
                 { (message !== "") ?
                     <div className="errorMessage">{message}</div> : <div className="errorMessage"> </div>}
-
-                <div id="message">
+                <div id="username_message">
+                    <h5>User name must be</h5>
+                    <div id="username_length" className="invalid">at least 6 characters long</div>
+                </div>
+                <div id="pswd_message">
                     <h5>Password must contain:</h5>
                     <div id="letter" className="invalid">A lowercase letter</div>
                     <div id="capital" className="invalid">A capital (uppercase) letter</div>
